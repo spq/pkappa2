de2a0865238e636b3072977f283e63fb60ada920
spq <spq@aachen.ccc.de>
2021-08-04 03:44:00 +0200

    add long bitmask class

---
+++ internal/tools/bitmask/longBitmask.go
@@ -0,0 +1,149 @@
+package bitmask
+
+import "math/bits"
+
+type (
+	LongBitmask struct {
+		mask []uint64
+	}
+)
+
+func (bm LongBitmask) IsSet(bit uint) bool {
+	idx, lbit := bit/64, bit%64
+	if idx < uint(len(bm.mask)) {
+		return (bm.mask[idx]>>lbit)&1 != 0
+	}
+	return false
+}
+
+func (bm LongBitmask) OnesCount() int {
+	count := 0
+	for _, m := range bm.mask {
+		count += bits.OnesCount64(m)
+	}
+	return count
+}
+
+func (bm LongBitmask) Len() int {
+	for idx := len(bm.mask) - 1; idx >= 0; idx-- {
+		m := bm.mask[idx]
+		if m != 0 {
+			return idx*64 + bits.Len64(m)
+		}
+	}
+	return 0
+}
+
+func (bm LongBitmask) IsZero() bool {
+	for _, m := range bm.mask {
+		if m != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (bm LongBitmask) LeadingZeros() int {
+	for idx, m := range bm.mask {
+		if m != 0 {
+			return idx*64 + bits.LeadingZeros64(m)
+		}
+	}
+	return -1
+}
+
+func (bm *LongBitmask) Set(bit uint) {
+	idx, lbit := bit/64, bit%64
+	if idx >= uint(len(bm.mask)) {
+		bm.mask = append(bm.mask, make([]uint64, uint(len(bm.mask)+1)-idx)...)
+	}
+	bm.mask[idx] |= 1 << lbit
+}
+
+func (bm *LongBitmask) Unset(bit uint) {
+	idx, lbit := bit/64, bit%64
+	if idx >= uint(len(bm.mask)) {
+		return
+	}
+	bm.mask[idx] &= ^(1 << lbit)
+}
+
+func (bm *LongBitmask) Flip(bit uint) {
+	idx, lbit := bit/64, bit%64
+	if idx >= uint(len(bm.mask)) {
+		bm.mask = append(bm.mask, make([]uint64, uint(len(bm.mask)+1)-idx)...)
+	}
+	bm.mask[idx] ^= 1 << lbit
+}
+
+func (bm LongBitmask) Equal(other LongBitmask) bool {
+	longer, shorter := bm.mask, other.mask
+	if len(longer) < len(shorter) {
+		longer, shorter = shorter, longer
+	}
+	for idx := range shorter {
+		if shorter[idx] != longer[idx] {
+			return false
+		}
+	}
+	for _, m := range longer[len(shorter):] {
+		if m != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func (bm *LongBitmask) Or(other LongBitmask) {
+	min := len(other.mask)
+	if min > len(bm.mask) {
+		min = len(bm.mask)
+	}
+	for idx := 0; idx < min; idx++ {
+		bm.mask[idx] |= other.mask[idx]
+	}
+	if len(bm.mask) < len(other.mask) {
+		bm.mask = append(bm.mask, other.mask[len(bm.mask):]...)
+	}
+}
+
+func (bm *LongBitmask) And(other LongBitmask) {
+	if len(bm.mask) > len(other.mask) {
+		bm.mask = bm.mask[:len(other.mask)]
+	}
+	for idx := range bm.mask {
+		bm.mask[idx] &= other.mask[idx]
+	}
+}
+
+func (bm *LongBitmask) Xor(other LongBitmask) {
+	min := len(other.mask)
+	if min > len(bm.mask) {
+		min = len(bm.mask)
+	}
+	for idx := 0; idx < min; idx++ {
+		bm.mask[idx] ^= other.mask[idx]
+	}
+	if len(bm.mask) < len(other.mask) {
+		bm.mask = append(bm.mask, other.mask[len(bm.mask):]...)
+	}
+}
+
+func (bm *LongBitmask) Sub(other LongBitmask) {
+	min := len(other.mask)
+	if min > len(bm.mask) {
+		min = len(bm.mask)
+	}
+	for idx := 0; idx < min; idx++ {
+		bm.mask[idx] &= ^other.mask[idx]
+	}
+}
+
+func (bm *LongBitmask) Shrink() {
+	for len(bm.mask) != 0 {
+		if bm.mask[len(bm.mask)-1] != 0 {
+			return
+		}
+		bm.mask = bm.mask[:len(bm.mask)-1]
+	}
+}
