a2cf34d789510d3695c619410b49b1a5b03d393e
spq <spq@aachen.ccc.de>
2024-06-30 21:20:08 +0200

    - fight more with typescript - add @types/moo - move @types/nearley to dev deps - exclude src/parser/query.ts from linting (it's generated by nearleyc)

--- web/src/parser/analyze.ts
+++ web/src/parser/analyze.ts
@@ -1,36 +1,41 @@
 import nearley, { Parser } from "nearley";
 import grammar from "./query";
 
-interface QueryElement {
+type QueryElement = {
   type: string;
   op?: string;
   expressions?: QueryElement[];
-  keyword?: {
-    value: string;
-  };
-  [key: string]: any;
+  expression?: QueryElement;
+  subquery_var?: moo.Token;
+  keyword?: moo.Token;
+  converter?: moo.Token;
+  value?: moo.Token;
 }
 
 interface QueryElementValue {
   pieces: { [key: string]: string };
   start: number;
   len: number;
-};
+}
 
 const queryGrammar: nearley.Grammar = nearley.Grammar.fromCompiled(grammar);
 
-export default function analyze(query: string): { [key: string]: QueryElementValue } {
+export default function analyze(query: string): {
+  [key: string]: QueryElementValue;
+} {
   const parser: Parser = new nearley.Parser(queryGrammar);
   try {
     parser.feed(query);
-  } catch (parseError: any) {
-    console.log("Error at character " + parseError.offset);
+  } catch (parseError) {
+    console.log(
+      `Error at character ${(parseError as { offset: number }).offset}`
+    );
     return {};
   }
 
   const result: { [key: string]: QueryElementValue } = {};
-  const elements: QueryElement[] = [...parser.results];
-  while (true) {
+  const elements = [...(parser.results as QueryElement[])];
+  for (;;) {
     const elem = elements.pop();
     if (elem === undefined) break;
     if (
@@ -41,24 +46,25 @@ export default function analyze(query: string): { [key: string]: QueryElementVal
       elements.push(...elem.expressions);
       continue;
     }
-    if (elem.type != "expression" || elem.keyword === undefined) continue;
+    if (elem.type == "expression" || elem.keyword === undefined) continue;
     if (!["sort", "limit", "ltime"].includes(elem.keyword.value)) continue;
-    let pieces: { [key: string]: string } = {};
-    var start: number | null = null;
-    var end: number | null = null;
+    const pieces: { [key: string]: string } = {};
+    let start: number | null = null;
+    let end: number | null = null;
     for (const [k, v] of Object.entries(elem)) {
       if (k == "type") continue;
       if (v == null) continue;
-      pieces[k] = v.value;
-      if (start == null || start > v.col) {
-        start = v.col;
+      const v2 = v as { value: string; col: number; text: string };
+      pieces[k] = v2.value;
+      if (start == null || start > v2.col) {
+        start = v2.col;
       }
-      if (end == null || end < v.col + v.text.length) {
-        end = v.col + v.text.length;
+      if (end == null || end < v2.col + v2.text.length) {
+        end = v2.col + v2.text.length;
       }
     }
     if (start == null || end == null) continue;
-    result[elem.keyword.value] = {pieces, start, len: end - start};
+    result[elem.keyword.value] = { pieces, start, len: end - start };
   }
   return result;
 }
