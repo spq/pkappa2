d352c458fa3b059a6e5d57bca7da35e0d37a22bf
Peace-Maker <peacemakerctf@gmail.com>
2026-04-18 12:58:12 +0200

    Config fixes for Typescript 6

--- web/env.d.ts
+++ web/env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="vite/client" />
+
+declare module "vuetify/styles";
+declare module "vuetify/styles/core";
+declare module "vuetify/styles/colors";
+declare module "vuetify/styles/utilities";
